2a0f3ca035e07a5a09671ad17b459ab7db5444cf
jp <jp@jpls-Inspiron-5480>
2021-03-31 16:58:15 -0300

    Projeto inicial

---
+++ livrariaRESTMVCJPA_inicial/src/main/java/br/edu/ifpb/dac/livraria/controlador/LivrosController.java
@@ -0,0 +1,42 @@
+package br.edu.ifpb.dac.livraria.controlador;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import br.edu.ifpb.dac.livraria.modelo.Livro;
+import br.edu.ifpb.dac.livraria.servico.LivroService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Controller
+@RequestMapping("/livros")
+public class LivrosController {
+	
+	@Autowired
+	private LivroService livroService;
+
+	@GetMapping
+	public String listar(Model model) {
+		model.addAttribute("livros", livroService.listaLivros());
+		return "/livros/livros";
+	}
+
+	public String detalhaLivro(Integer id) {
+		return "/livros/livro";
+	}
+	
+	public void adicionaLivro(Livro livro) {
+		
+		livroService.adicionaLivro(livro);
+	}
+	
+	public void atualizaLivro(Livro livro, Integer id) {
+		
+		livroService.atualizaLivro(id,livro);
+	}
+
+
+}
