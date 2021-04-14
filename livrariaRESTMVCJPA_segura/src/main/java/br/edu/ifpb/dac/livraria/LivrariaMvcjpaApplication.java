009a788f8a98f16d53c50837562c0f3c4563ceaa
jp <jp@jpls-Inspiron-5480>
2021-04-14 06:35:38 -0300

    Criação de projeto

---
+++ livrariaRESTMVCJPA_segura/src/main/java/br/edu/ifpb/dac/livraria/LivrariaMvcjpaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaMvcjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaMvcjpaApplication.class, args);
+	}
+
+}
---
+++ livrariaRESTMVCJPA_segura/src/main/java/br/edu/ifpb/dac/livraria/seguranca/SecurityConfig.java
@@ -0,0 +1,41 @@
+package br.edu.ifpb.dac.livraria.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+        	.and()
+        	.authorizeRequests()
+        	.antMatchers("/livros/*")
+        	.hasAuthority("USUARIO")
+            .anyRequest()
+            .permitAll()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(getJwtAuthoritiesConverter());
+    }
+
+	private JwtAuthenticationConverter getJwtAuthoritiesConverter() {
+		JwtGrantedAuthoritiesConverter jwac = new JwtGrantedAuthoritiesConverter();
+		jwac.setAuthoritiesClaimName("authorities");
+		jwac.setAuthorityPrefix("");
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		converter.setJwtGrantedAuthoritiesConverter(jwac);
+		return converter;
+	}
+
+}
