d02272d0311c2b38ecbda486975937807a6987ec
jp <jp@jpls-Inspiron-5480>
2021-04-14 19:01:00 -0300

    alteração

--- livrariaRESTMVCJPA_segura/src/main/java/br/edu/ifpb/dac/livraria/seguranca/SecurityConfig.java
+++ livrariaRESTMVCJPA_segura/src/main/java/br/edu/ifpb/dac/livraria/seguranca/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.edu.ifpb.dac.livraria.seguranca;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,12 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
-        	.and()
+        .and()
         	.authorizeRequests()
-        	.antMatchers("/livros/*")
-        	.hasAuthority("USUARIO")
-            .anyRequest()
-            .permitAll()
+        	.antMatchers(HttpMethod.GET,"/livros").hasAuthority("USUARIO")
+        	.antMatchers(HttpMethod.POST,"/livros").hasAuthority("ADMIN")
+        	.anyRequest().authenticated()
             .and()
             .oauth2ResourceServer()
             .jwt()
